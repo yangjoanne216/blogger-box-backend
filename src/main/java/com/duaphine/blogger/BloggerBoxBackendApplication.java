f58c534e427b6b95987ce778724c53c4d656b8e2
yangyang <yangjoanne216@gmail.com>
2024-04-22 14:39:00 +0200

    “COMMIT_MESSAGE”

---
+++ src/main/java/com/duaphine/blogger/BloggerBoxBackendApplication.java
@@ -0,0 +1,14 @@
+package com.duaphine.blogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerBoxBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BloggerBoxBackendApplication.class, args);
+	}
+
+}
